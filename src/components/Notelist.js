d2b63c98ecc4770fb73bf645718b4d2a1bd1a1ee
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2022-11-11 00:12:26 +0530

    added toggle view

--- src/components/Notelist.js
+++ src/components/Notelist.js
@@ -1,12 +1,12 @@
 import Note from "./Note"
 import PropTypes from 'prop-types'
 
-const Notelist = ({ Notes, onDelete, onEdit }) => {
+const Notelist = ({ Notes, onDelete, onEdit, toggleView }) => {
     return (
         <div className="notes-deck">
             {
                 (Notes).map((note) => (
-                    <Note key={note.id} Note={note} onDelete={onDelete} onEdit={onEdit} />
+                    <Note key={note.id} Note={note} onDelete={onDelete} onEdit={onEdit} toggleView={toggleView} />
                 ))
             }
         </div>
